d75b453ccd13e1b4f1c5e5e0676f19581ac06bbc
devmil <info@devmil.de>
2022-08-09 21:31:14 +0200

    adds some more tests for device_into_plus package

--- tool/test/integration_tests/device_info_plus_test.dart
+++ tool/test/integration_tests/device_info_plus_test.dart
@@ -57,6 +57,142 @@ void main() {
           expectedTypeName: 'int', expectedIsDeprecatedFlag: false);
       _testField(getHostClass, 'userName',
           expectedTypeName: 'String', expectedIsDeprecatedFlag: false);
+      test('does contain overwritten toMap() from super class', () {
+        expect(
+            getHostClass()
+                .executableDeclarations
+                .where((element) => element.name == 'toMap')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('contains constructor with right number of arguments', () {
+        final constructors = getHostClass()
+            .executableDeclarations
+            .where((element) => element.type == ExecutableType.constructor);
+        expect(constructors.length, 1);
+        expect(constructors.first.parameters.length, 25);
+        final requiredConstructorParameters = constructors.first.parameters
+            .where((element) => element.isRequired);
+        expect(requiredConstructorParameters.length, 25);
+      });
+    });
+    group('AndroidDeviceInfo is analyzed correctly', () {
+      // ignore: prefer_function_declarations_over_variables
+      final getHostClass = () {
+        return packageApi.classDeclarations
+            .firstWhere((element) => element.name == 'AndroidDeviceInfo');
+      };
+      _testField(getHostClass, 'board',
+          expectedTypeName: 'String?', expectedIsDeprecatedFlag: false);
+      _testField(getHostClass, 'device',
+          expectedTypeName: 'String?', expectedIsDeprecatedFlag: false);
+      _testField(getHostClass, 'host',
+          expectedTypeName: 'String?', expectedIsDeprecatedFlag: false);
+      _testField(getHostClass, 'isPhysicalDevice',
+          expectedTypeName: 'bool?', expectedIsDeprecatedFlag: false);
+      test('does contain overwritten toMap() from super class', () {
+        expect(
+            getHostClass()
+                .executableDeclarations
+                .where((element) => element.name == 'toMap')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('does contain BaseDeviceInfo super class', () {
+        expect(
+            getHostClass()
+                .superTypeNames
+                .where((element) => element == 'BaseDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('contains constructor with right number of arguments', () {
+        final constructors = getHostClass()
+            .executableDeclarations
+            .where((element) => element.type == ExecutableType.constructor);
+        expect(constructors.length, 1);
+        expect(constructors.first.parameters.length, 21);
+        final requiredConstructorParameters = constructors.first.parameters
+            .where((element) => element.isRequired);
+        expect(requiredConstructorParameters.length, 5);
+      });
+    });
+    group('Class existence tests', () {
+      test('MethodChannelDeviceInfo is not part of the public API', () {
+        // MethodChannelDeviceInfo gets used in the implementation but is not exposed directly or indirectly via
+        // any signatures => not part of the public API
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'MethodChannelDeviceInfo')
+                .isEmpty,
+            isTrue);
+      });
+      test('PlatformInterface is not part of the public API', () {
+        // PlatformInterface is used as a super class for DeviceInfoPlatform but does not belong to this package
+        // => it is not part of the public API
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'PlatformInterface')
+                .isEmpty,
+            isTrue);
+      });
+
+      test('DeviceInfoPlatform is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'DeviceInfoPlatform')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('BaseDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'BaseDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('WebBrowserInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'WebBrowserInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('MacOsDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'MacOsDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('WindowsDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'WindowsDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('LinuxDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'LinuxDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('IosDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'IosDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
+      test('AndroidDeviceInfo is part of the public API', () {
+        expect(
+            packageApi.classDeclarations
+                .where((element) => element.name == 'AndroidDeviceInfo')
+                .isNotEmpty,
+            isTrue);
+      });
     });
   });
 }
