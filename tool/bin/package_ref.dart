72aa8a44814510eca81e3977a4c0c50aa3662dd5
devmil <info@devmil.de>
2022-08-15 21:50:00 +0200

    first diff command implementation

---
+++ tool/bin/package_ref.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+class PackageRef {
+  final String ref;
+
+  PackageRef(this.ref);
+
+  bool get isPackageApiFile {
+    return File(ref).existsSync();
+  }
+
+  bool get isDirectoryPath {
+    return Directory(ref).existsSync();
+  }
+
+  bool get isPubRef {
+    final uri = Uri.parse(ref);
+    return uri.hasScheme && uri.scheme == 'pub';
+  }
+
+  String? get pubPackage {
+    if (!isPubRef) {
+      return null;
+    }
+    final uri = Uri.parse(ref);
+    return uri.host;
+  }
+
+  String? get pubVersion {
+    if (!isPubRef) {
+      return null;
+    }
+    final uri = Uri.parse(ref);
+    return uri.path.replaceAll('/', '');
+  }
+}
