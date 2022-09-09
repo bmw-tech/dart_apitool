4d00ce57dc379b9d36f80e22de4f73c6c97c22b5
devmil <info@devmil.de>
2022-09-09 12:20:17 +0200

    adds build_runner config to exclude generated files from 'non_constant_identifier_names' linter rule

--- lib/src/storage/v2/class_declaration_storage_v2.g.dart
+++ lib/src/storage/v2/class_declaration_storage_v2.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: non_constant_identifier_names
+
 part of 'class_declaration_storage_v2.dart';
 
 // **************************************************************************
