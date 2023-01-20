ffb2043c9a7269426efe9969970ca7b74d34297a
Michael Lamers <info@devmil.de>
2023-01-20 23:17:40 +0100

    Update lib/src/cli/commands/command_mixin.dart
    Co-authored-by: Adam <adamgic@wp.pl>

--- lib/src/cli/commands/command_mixin.dart
+++ lib/src/cli/commands/command_mixin.dart
@@ -77,7 +77,7 @@ Affects only local references.
     // merge sources to not copy children of a parent separately
     // => remove all sources that have a parent in the list
     sources.removeWhere((sToRemove) => sources.any(
-        (s) => s != sToRemove && p.isWithin(s.sourceDir, sToRemove.sourceDir)));
+        (s) => p.isWithin(s.sourceDir, sToRemove.sourceDir)));
     final tempDir = await Directory.systemTemp.createTemp();
     await Future.forEach<SourceItem>(sources, (sourceItem) async {
       stdout.writeln('Copying sources from ${sourceItem.sourceDir}');
