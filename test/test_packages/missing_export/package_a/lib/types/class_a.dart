9db8d026039e6c971bb19758e4700cb246261698
devmil <info@devmil.de>
2023-04-02 11:55:55 +0200

    adds test for detecting missing exports

---
+++ test/test_packages/missing_export/package_a/lib/types/class_a.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+import 'package:package_b/package_b.dart';
+
+import 'class_b.dart';
+
+@experimental
+class ClassA {
+  final ClassB classB;
+
+  ClassA({required this.classB});
+
+  String getName() {
+    return classB.getName();
+  }
+
+  ClassB getClassB() => ClassB('someValue');
+}
