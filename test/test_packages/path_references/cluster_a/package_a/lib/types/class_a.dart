40416bfdf8058afe8c5ccb135efac2fd4272337b
devmil <info@devmil.de>
2023-09-28 12:39:40 +0200

    fix: fixes local package refs by not adapting the relative path to the main package

--- test/test_packages/path_references/cluster_a/package_a/lib/types/class_a.dart
+++ test/test_packages/path_references/cluster_a/package_a/lib/types/class_a.dart
@@ -1,8 +1,9 @@
 import 'package:meta/meta.dart';
 import 'package:package_b/package_b.dart';
+import 'package:package_a/types/mixin_a.dart';
 
 @experimental
-class ClassA {
+class ClassA with MixinA {
   final ClassB classB;
 
   ClassA({required this.classB});
---
+++ test/test_packages/path_references/cluster_a/package_a/lib/types/mixin_a.dart
@@ -0,0 +1,3 @@
+mixin MixinA {
+  int thisIsAMixinField;
+}
