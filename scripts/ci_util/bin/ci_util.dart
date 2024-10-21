79264a6cb6003bd1a9b7c485ccab6e74c2a6b372
devmil <info@devmil.de>
2024-10-21 07:38:13 +0200

    dart format

--- scripts/ci_util/bin/ci_util.dart
+++ scripts/ci_util/bin/ci_util.dart
@@ -2,7 +2,8 @@ import 'package:args/command_runner.dart';
 import 'package:ci_util/src/check_lower_bound_dependencies_command.dart';
 
 CommandRunner buildCommandRunner() {
-  return CommandRunner('ci_util', 'CI utilities for dart_apitool')..addCommand(CheckLowerBoundDependenciesCommand());
+  return CommandRunner('ci_util', 'CI utilities for dart_apitool')
+    ..addCommand(CheckLowerBoundDependenciesCommand());
 }
 
 void main(List<String> arguments) async {
