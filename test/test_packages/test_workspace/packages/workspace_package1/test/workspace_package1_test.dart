c4b0f4c6ada0028bf5af36a326e8b983e40c2d1f
devmil <info@devmil.de>
2025-01-13 21:00:53 +0100

    bump Flutter to 3.27.1, add test workspace

---
+++ test/test_packages/test_workspace/packages/workspace_package1/test/workspace_package1_test.dart
@@ -0,0 +1,16 @@
+import 'package:workspace_package1/workspace_package1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
---
+++ test/test_packages/test_workspace/packages/workspace_package2/example/workspace_package2_example.dart
@@ -0,0 +1,6 @@
+import 'package:workspace_package2/workspace_package2.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
