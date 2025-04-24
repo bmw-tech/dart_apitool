bbb994f8d4046ed7749801bc99f64dc252821016
Michael Lamers <info@devmil.de>
2025-04-24 23:36:42 +0200

    ignore analyzer warnings of deprecated analyzer APIs

--- lib/src/model/internal/internal_field_declaration.dart
+++ lib/src/model/internal/internal_field_declaration.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:analyzer/dart/element/element.dart';
 
 import '../field_declaration.dart';
--- lib/src/model/internal/internal_type_usage.dart
+++ lib/src/model/internal/internal_type_usage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:analyzer/dart/element/element.dart';
 import 'package:dart_apitool/src/model/internal/internal_declaration_utils.dart';
 
