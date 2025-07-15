52661806ef729417a27a54e7bf6ee5e7d00bb645
Michael Lamers <info@devmil.de>
2025-07-15 16:06:41 +0200

    re-generate freezed with new version

--- lib/src/storage/v3/platform_constraints_storage_v3.freezed.dart
+++ lib/src/storage/v3/platform_constraints_storage_v3.freezed.dart
@@ -1,6 +1,5 @@
-// dart format width=80
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -79,6 +78,160 @@ class _$IOSPlatformConstraintsStorageV3CopyWithImpl<$Res>
           : minimumOsVersion // ignore: cast_nullable_to_non_nullable
               as num?,
     ));
+  }
+}
+
+/// Adds pattern-matching-related methods to [IOSPlatformConstraintsStorageV3].
+extension IOSPlatformConstraintsStorageV3Patterns
+    on IOSPlatformConstraintsStorageV3 {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_IOSPlatformConstraintsStorageV3 value)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_IOSPlatformConstraintsStorageV3 value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3():
+        return $default(_that);
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_IOSPlatformConstraintsStorageV3 value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(num? minimumOsVersion)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that.minimumOsVersion);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(num? minimumOsVersion) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3():
+        return $default(_that.minimumOsVersion);
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(num? minimumOsVersion)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _IOSPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that.minimumOsVersion);
+      case _:
+        return null;
+    }
   }
 }
 
@@ -250,6 +403,169 @@ class _$AndroidPlatformConstraintsStorageV3CopyWithImpl<$Res>
           : targetSdkVersion // ignore: cast_nullable_to_non_nullable
               as int?,
     ));
+  }
+}
+
+/// Adds pattern-matching-related methods to [AndroidPlatformConstraintsStorageV3].
+extension AndroidPlatformConstraintsStorageV3Patterns
+    on AndroidPlatformConstraintsStorageV3 {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_AndroidPlatformConstraintsStorageV3 value)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_AndroidPlatformConstraintsStorageV3 value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3():
+        return $default(_that);
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_AndroidPlatformConstraintsStorageV3 value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(
+            int? minSdkVersion, int? compileSdkVersion, int? targetSdkVersion)?
+        $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that.minSdkVersion, _that.compileSdkVersion,
+            _that.targetSdkVersion);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(
+            int? minSdkVersion, int? compileSdkVersion, int? targetSdkVersion)
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3():
+        return $default(_that.minSdkVersion, _that.compileSdkVersion,
+            _that.targetSdkVersion);
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(
+            int? minSdkVersion, int? compileSdkVersion, int? targetSdkVersion)?
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _AndroidPlatformConstraintsStorageV3() when $default != null:
+        return $default(_that.minSdkVersion, _that.compileSdkVersion,
+            _that.targetSdkVersion);
+      case _:
+        return null;
+    }
   }
 }
 
